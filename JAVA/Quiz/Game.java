c8f5fbc9fe28c39b8b0866f415fdc804db84796d
정콩순 <104056828+congs00n@users.noreply.github.com>
2022-04-29 09:30:29 +0900

    Update Game.java

--- JAVA/Quiz/Game.java
+++ JAVA/Quiz/Game.java
@@ -5,7 +5,7 @@ public class Game {
 
 		void gameTest() {
 			Scanner scanner = new Scanner(System.in);
-			int num = (int) (Math.random() * 10); // num = 0~9
+			int num = (int) (Math.random() * 10); // num = 1~9
 			for(int i = 1; i < 10; i ++) {
 				int input = scanner.nextInt();
 				
