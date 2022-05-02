d6f776cdcea2520f194b8904970b261570baab53
정콩순 <104056828+congs00n@users.noreply.github.com>
2022-05-02 15:29:40 +0900

    Update GameExe.java
    게임 횟수 수정

--- JAVA/Quiz/GameExe.java
+++ JAVA/Quiz/GameExe.java
@@ -6,7 +6,7 @@ public class GameExe {
 		Game test1 = new Game();
 		
 		System.out.println("소녀가 들고 있는 카드의 숫자는 1 ~ 9 중 하나입니다. ");
-		System.out.println("숫자를 맞춰보세요. (1 ~ 9 사이의 수 중 하나를 입력)");
+		System.out.println("숫자를 맞춰보세요. 기회는 다섯 번 입니다! ");
 		
 		test1.gameTest();
 		
